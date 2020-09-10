e6c73c1544b2cc04ee6c15c1ebc08319f19d7a48
Leonid Malanowski <LeonidM@priceindustries.com>
2020-09-09 22:30:35 -0500

    Connected to TMDB, added images, descriptions, likes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Nav from './Nav';
-import Results from './Results';
-
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Nav from "./Nav";
+import Results from "./Results";
 
 function App() {
   return (
     <div className="app">
-
       <Header></Header>
       <Nav></Nav>
       <Results></Results>
--- src/Results.js
+++ src/Results.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import './Results.css';
-import VideoCard from './VideoCard'
+import React, { useState, useEffect } from "react";
+import "./Results.css";
+import VideoCard from "./VideoCard";
+import axios from "./axios";
+import requests from "./requests";
 
 function Results() {
-    return (
-        <div className="results">
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-        </div>
-    );
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // run this code ONCE (with []) when the Results component loads / mounts
+    async function fetchData() {
+      const request = await axios.get(requests.fetchActionMovies);
+
+      setMovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="results">
+      {movies.map((movie) => (
+        <VideoCard movie={movie} />
+      ))}
+    </div>
+  );
 }
 
 export default Results;
